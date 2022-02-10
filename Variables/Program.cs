8caf5d9d706d8362280c61fc785f7336a961694d
Muhammed4 <namawulo2000@gmail.com>
2022-02-10 14:10:37 +0300

    ilk commit

---
+++ Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 99;
+            string isim = "muhamme şahar";
+            bool kar = 10 > 33;
+
+            DateTime zaman = DateTime.Now;
+
+            Console.WriteLine("değişkenlerimiz\n " + " \n "+ num + "\n  " + isim + "\n  " + kar + " \n " + zaman);
+            Console.ReadKey();
+        }
+    }
+}
